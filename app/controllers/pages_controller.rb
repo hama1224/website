76e45deadfa368358726eaeda3a159fcd4b5d6f6
hama1224 <hama.k1224@gmail.com>
2020-01-31 15:08:41 +0000

    項目追加

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,24 @@
 class PagesController < ApplicationController
   def index
-    
   end
+  
+  def company_profile
+  end
+  
+  def description
+  end
+  
+  def performance
+  end
+  
+  def recruit
+  end
+  
+  def contact
+  end
+  
+  def blog
+  end
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root 'pages#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'pages/company_profile'
+  get 'pages/description'
+  get 'pages/performance'
+  get 'pages/recruit'
+  get 'pages/contact'
+  get 'pages/blog'
+  
   
 end
