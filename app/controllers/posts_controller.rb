46ee29b668017e6229af7550ba0d43b6cf337684
hama1224 <hama.k1224@gmail.com>
2020-02-12 13:06:04 +0000

    簡単な投稿機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -17,8 +18,20 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    post = Post.find(params[:id])
+    post.update!(post_params)
+    redirect_to posts_url, notice: "投稿「#{post.title}」を更新しました。"
   end
 
+def destroy
+  post = Post.find(params[:id])
+  post.destroy
+  redirect_to posts_url, notice: "投稿「#{post.title}」を削除しました。"
+end
 
  private
   def post_params
