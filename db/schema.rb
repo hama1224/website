eef17ab3fa09dfebdbfd28a7ee86f58996c45dc7
hama1224 <hama.k1224@gmail.com>
2020-02-16 05:24:00 +0000

    パスワード

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_11_100706) do
+ActiveRecord::Schema.define(version: 2020_02_16_051234) do
 
   create_table "blogconfigs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "title"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 2020_02_11_100706) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
